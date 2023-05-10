a920e9deb032ca1fd0e98824f55e375ce3ef5ec7
Mason Stewart <masonstewart2276@gmail.com>
2023-05-09 21:12:27 -0700

    third commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,15 +2,37 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import CardItem from './CardItem';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='./videos/video-1.mp4' autoPlay loop muted />
       <div className='hero-profile'>
-        <img src='./public/logo192.png'/>
-      <h1>MASON STEWART</h1>
-      <p>Nam venenatis sem a lectus ultricies lobortis. Nunc finibus et metus eu blandit. Curabitur bibendum pharetra nulla, ac malesuada tortor varius nec. Duis at lorem est. Quisque elementum, justo porttitor maximus facilisis, quam magna</p>
+        <ul className='cards__items'>
+            <CardItem 
+            src='./images/profilepic.png'
+            h1='MASON STEWART'
+            text='Nam venenatis sem a lectus ultricies lobortis. Nunc finibus et metus eu blandit. Curabitur bibendum pharetra nulla, ac malesuada tortor varius nec. Duis at lorem est. Quisque elementum, justo '
+            />
+        </ul>
+      </div>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
       </div>
     </div>
   );
