8af62bbb550e70d646b0efb7c536d1438b5fdbb5
Mason Stewart <masonstewart2276@gmail.com>
2023-05-11 20:51:12 -0700

    fourth commit

---
+++ src/components/HeroSection1.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection1.css';
+import CardItem1 from './CardItem1';
+
+function HeroSection1() {
+  return (
+    <div className='hero-container'>
+      <video src='./videos/video-1.mp4' autoPlay loop muted />
+        <h1>Front End Software Developer</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie nunc eu odio tempus, et suscipit enim viverra. Sed nec odio iaculis, commodo augue a, egestas neque. </p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection1;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
-
+import HeroSection1 from '../HeroSection1';
 
 function About() {
   return (
     <>
+    <HeroSection1 />
     <HeroSection/>
     </>
   );
